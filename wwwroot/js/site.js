0d055453faa4f5ce100a7be6d1b11d3393f08298
mauricio <mauricio2017rubiale@gmail.com>
2021-04-13 17:57:31 -0300

    Login quase pronto

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,16 @@
-﻿
+﻿$("#logar").bind("click", function(){
+
+    var dadosLogin = $("#insert-login").serialize()
+
+    $.ajax({
+        type: "POST",
+        url: "Login/Login",
+        data: dadosLogin,
+        success: function (response) {
+            console.log(response)
+        },
+        error: function(){
+            alert("Error!")
+        }
+    });
+})
